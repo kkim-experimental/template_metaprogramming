d3d3f2f1ea336d620f543388ec8b39be77e65aef
k <kyungminkim7@gmail.com>
2020-06-27 06:17:13 -0500

    Added test functionality

---
+++ basics/include/basics/max1.h
@@ -0,0 +1,6 @@
+#pragma once
+
+template<typename T>
+T max(T a, T b) {
+    return b < a ? a : b;
+}
--- basics/src/main.cpp
+++ basics/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+#include <basics/max1.h>
+
 int main(int argc, char *argv[]) {
-    std::cout << "Test\n";
+    std::cout << max(4.5, 3.2) << "\n";
     return 0;
 }
