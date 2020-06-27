79ec0acb7d73931d057d803f03e48a96ee24b09b
k <kyungminkim7@gmail.com>
2020-06-27 07:01:33 -0500

    Added testing framework using googletest

--- basics/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include <basics/max1.h>
-
-int main(int argc, char *argv[]) {
-    std::cout << max(4.5, 3.2) << "\n";
-    return 0;
-}
---
+++ basics/test/max1.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include <basics/max1.h>
+
+TEST(Max1, testInt) {
+    EXPECT_TRUE(true);
+}
